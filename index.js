96c396353c5b4a11b63a4ea13d2b080a419305f9
Andrew Virts <asvirts@outlook.com>
2022-08-07 10:04:52 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,52 @@
+let homeScore = 0
+let awayScore = 0
+document.getElementById('home-score').innerHTML
+document.getElementById('away-score').innerHTML
+
+function homeScore1() {
+    element = document.getElementById("home-score");
+    let newHomeScore = homeScore + 1
+    homeScore = newHomeScore
+    element.innerHTML = homeScore;
+    console.log("Crimson Tide score! +1")
+}
+
+function homeScore2() {
+    element = document.getElementById("home-score");
+    let newHomeScore = homeScore + 2
+    homeScore = newHomeScore
+    element.innerHTML = homeScore;
+    console.log("Crimson Tide score! +2")
+}
+
+function homeScore3() {
+    element = document.getElementById("home-score");
+    let newHomeScore = homeScore + 3
+    homeScore = newHomeScore
+    element.innerHTML = homeScore;
+    console.log("Crimson Tide score! +3")
+}
+
+function awayScore1() {
+    element = document.getElementById("away-score");
+    let newAwayScore = awayScore + 1
+    awayScore = newAwayScore
+    element.innerHTML = awayScore;
+    console.log("Away team score +1, boo!")
+}
+
+function awayScore2() {
+    element = document.getElementById("away-score");
+    let newAwayScore = awayScore + 2
+    awayScore = newAwayScore
+    element.innerHTML = awayScore;
+    console.log("Away team score +2, boo!")
+}
+
+function awayScore3() {
+    element = document.getElementById("away-score");
+    let newAwayScore = awayScore + 3
+    awayScore = newAwayScore
+    element.innerHTML = awayScore;
+    console.log("Away team score +3, boo!")
+}
