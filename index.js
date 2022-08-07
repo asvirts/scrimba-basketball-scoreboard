4fb99741f48fbf39d33dda9c9dbafd92aa9fa848
Andrew Virts <asvirts@outlook.com>
2022-08-07 10:21:51 -0500

    added new game button

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ let awayScore = 0
 document.getElementById('home-score').innerHTML
 document.getElementById('away-score').innerHTML
 
+
+// Change home score
+
 function homeScore1() {
     element = document.getElementById("home-score");
     let newHomeScore = homeScore + 1
@@ -27,6 +30,9 @@ function homeScore3() {
     console.log("Crimson Tide score! +3")
 }
 
+
+// Change away score
+
 function awayScore1() {
     element = document.getElementById("away-score");
     let newAwayScore = awayScore + 1
@@ -49,4 +55,20 @@ function awayScore3() {
     awayScore = newAwayScore
     element.innerHTML = awayScore;
     console.log("Away team score +3, boo!")
+}
+
+
+// Bottom buttons
+
+function newGame() {
+    homeScore = 0
+    awayScore = 0
+    
+    element = document.getElementById("home-score");
+    element.innerHTML = homeScore;
+
+    element = document.getElementById("away-score");
+    element.innerHTML = awayScore;
+
+    console.log("New game!")
 }
